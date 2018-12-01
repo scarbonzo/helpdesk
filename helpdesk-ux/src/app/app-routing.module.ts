418507f26a9b18be2ded2a9158f281ab11ec6259
Rich Eodice <rich@eodice.com>
2018-12-01 13:07:29 -0500

    Created bare components, navbar and routing.

--- helpdesk-ux/src/app/app-routing.module.ts
+++ helpdesk-ux/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TicketsComponent } from './tickets/tickets.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ReportsComponent } from './reports/reports.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'tickets',
+    component: TicketsComponent
+  },
+  {
+    path: 'reports',
+    component: ReportsComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- helpdesk-ux/src/app/app.module.ts
+++ helpdesk-ux/src/app/app.module.ts
@@ -1,18 +1,33 @@
+import { HelpdeskService } from './helpdesk.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ReportsComponent } from './reports/reports.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    TicketsComponent,
+    SettingsComponent,
+    ReportsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HelpdeskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
